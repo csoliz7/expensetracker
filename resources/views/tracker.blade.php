07c1f0d8d1e520c982ba6ae8802f35953f68ac6c
Carlos Soliz <csoliz7@yahoo.com>
2015-02-20 15:56:37 -0800

    Using Eloquent ORM Created new controller Testing stages

--- resources/views/tracker.blade.php
+++ resources/views/tracker.blade.php
@@ -3,10 +3,35 @@
 @section('content')
 <h1>Expense Tracker</h1>
 
-<form method="post" action="store" >
+{{ print_r($restaurants) }}
+
+
+
+<form method="post" action="billtracker.local/index.php/store" >
     <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
 
-<div class="row">
+
+    <div class="row">
+        <div class="medium-6 columns">
+            <label>Select Restaurant:
+                <select name="restaurant">
+                    <option value="jerrys">Jerrys Deli</option>
+                    <option value="ihop">IHOP</option>
+                    <option value="Wendys">Wendys</option>
+                    <option value="panda">Panda Express</option>
+                    <option value="marie-callender">Marie Callenders</option>
+                    <option value="other">Other</option>
+
+                </select>
+            </label>
+        </div>
+    </div>
+
+
+
+
+
+    <div class="row">
     <div class="medium-6 columns">
         <label>Enter Bill Amount:
             <input type="number" step="any" name="amount" placeholder="Enter amount" />
@@ -23,21 +48,6 @@
     </div>
 </div>
 
-
-<div class="row">
-    <div class="medium-6 columns">
-        <label>Select Restaurant:
-            <select name="restaurant">
-                <option value="jerrys">Jerrys Deli</option>
-                <option value="ihop">IHOP</option>
-                <option value="Wendys">Wendys</option>
-                <option value="panda">Panda Express</option>
-                <option value="marie-callender">Marie Callenders</option>
-
-            </select>
-        </label>
-    </div>
-</div>
 
 <div class="row">
     <div class="medium-6 columns">
